b99aaafe4ef1a87058b464674a2e6fda97cc63df
hoyajigi <hoyajigi@gmail.com>
2019-10-14 22:41:41 +0900

    Add Junit 5

---
+++ src/test/java/kakaotalkfriendssort/MiscellaneousTests.java
@@ -0,0 +1,13 @@
+package kakaotalkfriendssort;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class MiscellaneousTests {
+    @Test
+    @DisplayName("1 + 1 = 2")
+    void addsTwoNumbers() {
+        assertEquals(2, 1 + 1, "1 + 1 should equal 2");
+    }}
